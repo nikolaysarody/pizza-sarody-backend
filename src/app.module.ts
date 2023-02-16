06c4822c925ab51c25e87275dd45b79a71edf07b
Sayansky Nikolay <sarody@ya.ru>
2023-02-16 20:06:53 +0800

    init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {PizzaController} from './pizza/pizza.controller';
+import {AuthController} from './auth/auth.controller';
+import {AuthModule} from './auth/auth.module';
+import {PizzaModule} from './pizza/pizza.module';
+import {ActionModule} from './action/action.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot('mongodb://admin:admin@127.0.0.1:27017/pizza'),
+        AuthModule,
+        PizzaModule,
+        ActionModule
+    ],
+    controllers: [AppController, PizzaController, AuthController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
