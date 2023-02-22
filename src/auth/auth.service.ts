9cbfe8d03f9fd2cd78d6e1e438466245a1abdba2
Sayansky Nikolay <sarody@ya.ru>
2023-02-22 10:41:47 +0800

    jwt token and auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,50 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {Model} from 'mongoose';
 import {User, UserDocument} from './schemas/user.schema';
 import {AuthDto} from './dto/auth.dto';
-import {genSaltSync, hashSync} from 'bcryptjs';
+import {compare, genSalt, hash} from 'bcryptjs';
+import {USER_NOT_FOUND_ERROR, WRONG_PASSWORD_ERROR} from './auth.constants';
+import {JwtService} from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
-    }
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
+        private readonly jwtService: JwtService
+    ) { }
 
     async createUser(dto: AuthDto): Promise<User> {
-        const salt = genSaltSync(10);
+        const salt = await genSalt(10);
         const newUser = new this.userModel({
             _id: Date.now().toString(),
             email: dto.login,
-            passwordHash: hashSync(dto.password, salt),
+            passwordHash: await hash(dto.password, salt),
             createdAt: new Date()
         });
         return newUser.save();
     }
 
-    async findUser(email: string) {
+    async findUser(email: string): Promise<User> {
         return this.userModel.findOne({email}).exec();
+    }
+
+    async validateUser(email: string, password: string): Promise<Pick<User, 'email'>> {
+        const user = await this.findUser(email);
+        if (!user) {
+            throw new UnauthorizedException(USER_NOT_FOUND_ERROR);
+        }
+        const isCorrectPassword = await compare(password, user.passwordHash);
+        if (!isCorrectPassword) {
+            throw new UnauthorizedException(WRONG_PASSWORD_ERROR);
+        }
+        return {email: user.email};
+    }
+
+    async login(email: string) {
+        const payload = {email};
+        return {
+            access_token: await this.jwtService.signAsync(payload)
+        }
     }
 }
