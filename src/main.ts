ce098ac4cdd7df4f39bf32ba17a14bf3325b90d4
Nikolay <sarody@ya.ru>
2023-07-03 08:10:57 +0800

    cors fix for all api

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe);
     app.setGlobalPrefix('api');
     app.enableCors({
-        origin: 'http://localhost:3000',
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         credentials: true
     });
 
